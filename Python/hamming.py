a1d9daf692a34e784b91cdc27c317af2486af97a
MountainMan12 <pawan12394@gmail.com>
2020-07-15 11:35:05 +0530

    Added Hamming.py Added data

---
+++ Python/hamming.py
@@ -0,0 +1,20 @@
+#Python program to find hamming distance between 2 DNA sequences
+
+fh = open('D:\\BIOINFORMATICS STUDY\\Rosalind Codes\\data\\rosalind_hamm.txt','r')
+
+if(fh.mode == 'r'):
+    dna = fh.read().split('\n')
+
+
+seq1 = dna[0]
+seq2 = dna[1]
+
+dis = 0
+for i in range(0, len(seq1)):
+
+    if(seq1[i] == seq2[i]):
+        continue
+    else:
+        dis += 1
+
+print(dis)
